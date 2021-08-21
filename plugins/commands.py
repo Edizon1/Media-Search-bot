7d1726068f2db8e23ca63fbfb7027bfd0e5d9dc9
Edizon1 <86514881+Edizon1@users.noreply.github.com>
2021-08-21 22:32:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,7 +100,7 @@ async def start(bot, cmd):
                 [
                     [
                         InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Other Bots", url="https://t.me/joinchat/0Z2-iPybfDcyYjg1")
+                        InlineKeyboardButton("Developer 2", url="https://t.me/Edizon1")
                     ],
                     [
                         InlineKeyboardButton("About", callback_data="about")
@@ -192,8 +192,8 @@ async def delete(bot, message):
 async def bot_info(bot, message):
     buttons = [
         [
-            InlineKeyboardButton('Update Channel', url='https://t.me/subin_works'),
-            InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
+            InlineKeyboardButton('Update Channel', url='https://t.me/TGmovie9'),
+            
         ]
         ]
-    await message.reply(text="<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text="<b>Hi I am Cassandra Raill 😉 I am familiar to those who Watched  12 monkeys Series🐵" </b>, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
