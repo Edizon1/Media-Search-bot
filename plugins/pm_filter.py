e7ed6b2a00bed75d91e49cd6deef202c4ccbd2bd
Edizon1 <86514881+Edizon1@users.noreply.github.com>
2021-08-21 22:25:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -272,12 +272,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 return
         elif query.data == "about":
             buttons = [
-                [
-                    InlineKeyboardButton('Update Channel', url='https://t.me/subin_works'),
-                    InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
+                
+                    InlineKeyboardButton('Update Channel', url='https://t.me/TGmovie9'),
+                    
                 ]
-                ]
-            await query.message.edit(text="<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+                
+            await query.message.edit(text="<b>Hi I am Cassandra Raill 😉 I am familiar to those who Watched  12 monkeys Series🐵 </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
 
 
