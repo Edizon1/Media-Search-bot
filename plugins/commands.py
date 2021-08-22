cd88a3d2289b45632388c6d8a9915f9c8622cce9
Edizon1 <86514881+Edizon1@users.noreply.github.com>
2021-08-22 13:41:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -197,4 +197,4 @@ async def bot_info(bot, message):
         ]
         ]
   
-message.reply(text="<b>Hi I am Cassandra Raill 😉 I am familiar to those who Watched  12 monkeys Series🐵 </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+await message.reply(text="<b>Hi I am Cassandra Raill 😉 I am familiar to those who Watched  12 monkeys Series🐵 </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
